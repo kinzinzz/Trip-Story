007d6e9730062b2d91132878ee366a5544f5046c
LEE DONG YEONG <kinzinzz@naver.com>
2022-11-01 16:07:36 +0900

    이동영 커밋

--- PJT/reviews/urls.py
+++ PJT/reviews/urls.py
@@ -4,6 +4,16 @@ from . import views
 app_name = "reviews"
 
 urlpatterns = [
-    path('review/', views.review, name='review'),
-    path('review_create/', views.review_create, name='review_create'),
+    # 리뷰 인덱스
+    path("", views.index, name="index"),
+    # 리뷰 생성
+    path("create/", views.create, name="create"),
+    # 리뷰 상세
+    path("<int:review_pk>/", views.detail, name="detail"),
+    # 리뷰 수정
+    path("<int:review_pk>/update/", views.update, name="update"),
+    # 리뷰 삭제
+    path("<int:review_pk>/delete/", views.delete, name="delete"),
+    # 리뷰 좋아요
+    path("<int:review_pk>/like/", views.like, name="like"),
 ]
--- PJT/reviews/views.py
+++ PJT/reviews/views.py
@@ -1,26 +1,108 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from . import models
+from . import forms
+from django.http import JsonResponse
 
-from django.shortcuts import render, redirect
-from .models import Review
-from .forms import ReviewForm
 
-# Create your views here.
-def review(request):
-    reviews = Review.objects.order_by('-pk')
-    context = {
-        'reviews':reviews,
-    }
-    return render(request, 'reviews/review.html', context)
+# 리뷰 인덱스
+def index(request):
+    # 베스트3 리뷰 context
 
-def review_create(request):
-    if request.method == 'POST':
-        review_form = ReviewForm(request.POST, request.FILES)
-        if review_form.is_valid():
-            review = review_form.save(commit=False)
+    # like 많은순으로 정렬하고 0~2등 가져오기
+    # ??
+    # reviews = models.Review.objects.all().order_by("-like.count()")[:2]
+    return render(
+        request,
+        "reviews/index.html",  # {'reviews':reviews}
+    )
+
+
+# 리뷰 생성
+def create(request):
+
+    if request.method == "POST":
+        form = forms.ReviewForm(request.POST, request.FIELS)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
             review.save()
-        return redirect('reviews:review')
+
+            return redirect("reviews:index")
+
     else:
-        review_form = ReviewForm()
-    context = {
-        'review_form': review_form,
-    }
-    return render(request, 'reviews/review_create.html',context)
+        form = forms.ReviewForm()
+
+    return render(request, "reviews/create.html", {"form": form})
+
+
+# 리뷰 상세페이지
+def detail(request, review_pk):
+
+    review = get_object_or_404(models.Review, pk=review_pk)
+
+    return render(request, "reviews/detail", {"review": review})
+
+
+# 리뷰 수정
+def update(request, review_pk):
+    review = get_object_or_404(models.Review, pk=review_pk)
+
+    if request.method == "POST":
+        form = forms.ReviewForm(request.POST, request.FIELS, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect("reviews:index")
+
+    else:
+        form = forms.ReviewForm(instance=review)
+
+    return render(request, "reviews/update.html", {"form": form})
+
+
+# 리뷰 삭제
+def delete(request, review_pk):
+    review = get_object_or_404(models.Review, pk=review_pk)
+
+    review.delete()
+
+    return redirect("reviews:index")
+
+
+#  리뷰 좋아요
+def like(request, review_pk):
+
+    review = get_object_or_404(models.Review, pk=review_pk)
+
+    if request.user in review.like.all():
+        review.like.remove(request.user)
+    else:
+        review.like.add(request.user)
+
+    likeCount = review.like.count()
+
+    return render(
+        request,
+        "reviews/detail.html",
+        {
+            "likeCount": likeCount,
+        },
+    )
+
+
+# 좋아요 기능 비동기
+# if request.user in review.like.all():
+#     review.like.remove(request.user)
+#     # request.user가 이전에 좋아요를 클릭한 사람이라면 좋아요 취소
+#     existed_user = False
+
+# else:
+#     review.like.add(request.user)
+#     # request.user가 이전에 좋아요를 클릭한 사람이 아니라면 좋아요
+#     existed_user = True
+# likeCount = review.like.count()
+# context = {
+#     "existed_user": existed_user,
+#     "likeCount": likeCount,
+# }
+
+# return JsonResponse(context)
