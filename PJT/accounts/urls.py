6a1d012ffe27092b5f1e6b19db78c24bf1edd230
pangpyo <kkp0639@gmail.com>
2022-11-01 15:45:26 +0900

    account first commit

--- PJT/accounts/urls.py
+++ PJT/accounts/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 app_name = "accounts"
 
-urlpatterns = []
+urlpatterns = [
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("<int:pk>", views.profile, name="profile"),
+    path("update/", views.update, name="update"),
+]
