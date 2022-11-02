23487b4b7baf926a894eb0911243eaa15b24eb28
pangpyo <kkp0639@gmail.com>
2022-11-03 02:19:55 +0900

    comment star

--- PJT/places/views.py
+++ PJT/places/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import City, Spot
+from .models import City, Spot, Spotcomment
 from .forms import CityForm, SpotForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
 
 # Create your views here.
 
@@ -49,10 +50,42 @@ def citycreate(request):
 
 def spot(request, cityname, pk):
     spot = Spot.objects.get(pk=pk)
+    comments = Spotcomment.objects.filter(spot=spot)
     comment_form = CommentForm()
+    grade = comments.aggregate(avg=Avg("grade"))
+    avg = "리뷰 없음"
+    star = ""
+    if grade["avg"]:
+        if grade["avg"] > 4.9:
+            star = "★★★★★"
+        elif grade["avg"] > 4.4:
+            star = "★★★★☆"
+        elif grade["avg"] > 3.9:
+            star = "★★★★"
+        elif grade["avg"] > 3.4:
+            star = "★★★☆"
+        elif grade["avg"] > 2.9:
+            star = "★★★"
+        elif grade["avg"] > 2.4:
+            star = "★★☆"
+        elif grade["avg"] > 1.9:
+            star = "★★"
+        elif grade["avg"] > 1.4:
+            star = "★☆"
+        elif grade["avg"] > 0.9:
+            star = "★"
+        elif grade["avg"] > 0.4:
+            star = "☆"
+        else:
+            avg = ""
+        avg = round(grade["avg"], 1)
+    comments = Spotcomment.objects.filter(spot=spot)
     context = {
         "spot": spot,
         "comment_form": comment_form,
+        "comments": comments,
+        "avg": avg,
+        "star": star,
     }
     return render(request, "places/spot.html", context)
 
@@ -88,4 +121,4 @@ def spotcomment(request, cityname, pk):
         comment.spot = spot
         comment.user = request.user
         comment.save()
-    return redirect("articles:detail", cityname, pk)
+    return redirect("places:spot", cityname, pk)
