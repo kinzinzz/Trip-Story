afbc92e54311c12b912c8003ee494bd22e1e4636
pangpyo <kkp0639@gmail.com>
2022-11-07 18:20:30 +0900

    Commit Message

--- PJT/PJT/settings.py
+++ PJT/PJT/settings.py
@@ -26,9 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-secret_file = os.path.join(BASE_DIR, "secrets.json")
-
-
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
